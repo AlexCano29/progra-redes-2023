4f5e747f19c2b74e4ba3f084046376a948a48af1
AlexCano29 <138088668+AlexCano29@users.noreply.github.com>
2023-09-18 11:01:16 -0600

    Add files via upload

---
+++ 2.1.1.19.py
@@ -0,0 +1,36 @@
+'''
+Autor: Alexis Cano
+Fecha 18 septiembre
+Descripcion: Iniciando con Python
+
+Lab:2.1.1.19
+'''
+
+
+print("    *     " *2)
+print("   * *    " *2)
+print("  *   *   " *2)
+print(" *     *  " *2)
+print("***   *** " *2)
+print("  *   *   " *2)
+print("  *   *   " *2)
+print("  *****   " *2)
+
+print("    *\n* *\n*   *\n *     *\n***   ***\n  *   *\n  *   *   \n*****   " )
+
+print("    **")
+print("    **")
+print("   ****")
+print("   ****")
+print("  **  **")
+print("  **  **")
+print(" **    **")
+print(" **    **")
+print("****  ****")
+print("****  ****")
+print("  **   **")
+print("  **   **")
+print("  **   **")
+print("  **   **")
+print("  *******")
+print("  *******")
